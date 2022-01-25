07b6f08b7fef5d86c65ee33794b1b7ccaffa103c
Jay <92544545+jayprajapati9@users.noreply.github.com>
2022-01-25 12:14:48 +0530

    Frontend Ui Completed

---
+++ Frontend/js/script.js
@@ -0,0 +1,6 @@
+
+let floatMenu = document.querySelector(".FLOATMENU");
+let verticalDots = document.querySelector(".bx-dots-vertical-rounded");
+verticalDots.addEventListener("click", () => {
+    floatMenu.classList.toggle("hidden");
+});
