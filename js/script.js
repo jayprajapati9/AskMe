8d5585fea077f54665a34052637d4c64e2d39949
Jay <92544545+jayprajapati9@users.noreply.github.com>
2022-02-01 11:50:12 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-
-let floatMenu = document.querySelector(".FLOATMENU");
-let verticalDots = document.querySelector(".bx-dots-vertical-rounded");
-verticalDots.addEventListener("click", () => {
-    floatMenu.classList.toggle("hidden");
-});
